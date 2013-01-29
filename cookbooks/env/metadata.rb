53ddf9ca5cbf1ed5c7fd424734dfc0e184b62fca
Seyo <olofsson.e@gmail.com>
2013-01-29 12:19:19 +0000

    Adding env cookbook

---
+++ cookbooks/env/metadata.rb
@@ -0,0 +1,25 @@
+maintainer       "CGI"
+maintainer_email "emil.olofsson@cgi.com"
+license          "All rights reserved"
+description      "Installs/Configures env"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "rightscale"
+
+recipe "env::create_folder","Creates folder and add file"
+
+# Required Input#
+
+attribute "env/folder_name",
+          :display_name => "Folder Name",
+          :description => "The full folder path (/opt/environment/archive/)",
+          :required => "required",
+          :recipes => ["env::folder_create"]
+
+attribute "env/file_name",
+          :display_name => "File Name",
+          :description => "The name of the file)",
+          :required => "required",
+          :recipes => ["env::folder_create"]
+
---
+++ cookbooks/env/recipes/create_folder.rb
@@ -0,0 +1,8 @@
+rightscale_marker :begin
+
+folder_name = node[:env][:folder_name]
+file_name = node[:env][:file_name]
+
+log "Scanning: #{folder_name}#{file_name}"
+
+rightscale_maker :end

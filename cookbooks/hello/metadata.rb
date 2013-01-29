4733882452669b59f146664bf2d18f4118c187df
Seyo <olofsson.e@gmail.com>
2013-01-29 11:39:18 +0000

    ops, rigthscale isntad of rightscale

--- cookbooks/hello/metadata.rb
+++ cookbooks/hello/metadata.rb
@@ -5,6 +5,6 @@ description      "Will print a silly message in the output"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
 
-depends "rigthscale"
+depends "rightscale"
 
 recipe "hello::default","Print things in output"
